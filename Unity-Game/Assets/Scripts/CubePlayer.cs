9146cbd38f888c09b9d2f2db7aaea572c0f460d1
b14nk3r <b1anker.kr@gmail.com>
2023-01-26 20:28:42 +0900

    플레이어 무기 줍기, 교체 추가

--- Unity-Game/Assets/Scripts/CubePlayer.cs
+++ Unity-Game/Assets/Scripts/CubePlayer.cs
@@ -4,22 +4,95 @@ using UnityEngine;
 
 public class CubePlayer : MonoBehaviour
 {
+    public GameObject[] weapons;
+    public bool[] hasWeapon;
     public float speed;
-    float h;
-    float v;
+    float hAxis;
+    float vAxis;
     Vector3 moveVec;
+
+    GameObject nearObject;
+    GameObject equipWeapon;
+    int equipWeaponIndex = -1;
+
+    bool iDown;
+    bool sDown1;
+    bool sDown2;
 
 void Start(){
     
 }
 
     void Update(){
-        h = Input.GetAxisRaw("Horizontal");
-        v = Input.GetAxisRaw("Vertical");
+        GetInput();
+        Move();
+        Swap();
+        Interation();
+    }
 
-        moveVec = new Vector3(h, 0, v).normalized;
+    void GetInput()
+    {
+        hAxis = Input.GetAxisRaw("Horizontal");
+        vAxis = Input.GetAxisRaw("Vertical");
+        iDown = Input.GetButtonDown("Interation");
+        sDown1 = Input.GetButtonDown("Swap1");
+        sDown2 = Input.GetButtonDown("Swap2");
+    }
+
+    void Move()
+    {
+        moveVec = new Vector3(hAxis, 0, vAxis).normalized;
         transform.position += moveVec * speed * Time.deltaTime;
 
         transform.LookAt(transform.position + moveVec);
+    }
+
+    void Swap()
+    {
+        if (sDown1 && (!hasWeapon[0] || equipWeaponIndex == 0))
+            return;
+        if (sDown2 && (!hasWeapon[1] || equipWeaponIndex == 1))
+            return;
+
+        int weaponIndex = -1;
+        if (sDown1) weaponIndex = 0;
+        if (sDown2) weaponIndex = 1;
+
+        if (sDown1 || sDown2)
+        {
+            if (equipWeapon != null)
+                equipWeapon.SetActive(false);
+            equipWeaponIndex = weaponIndex;
+            equipWeapon = weapons[weaponIndex];
+            equipWeapon.SetActive(true);
+        }
+    }
+
+    void Interation()
+    {
+        if(iDown && nearObject != null)
+        {
+            if(nearObject.tag == "Weapon")
+            {
+                Item item = nearObject.GetComponent<Item>();
+                int weaponIndex = item.value;
+                hasWeapon[weaponIndex] = true;
+
+                Destroy(nearObject);
+            }
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Weapon")
+            nearObject = other.gameObject;
+        Debug.Log("æ∆¿Ã≈€ ¥Í¿Ω");
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Weapon")
+            nearObject = null;
     }
 }
